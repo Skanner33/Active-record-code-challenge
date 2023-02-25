2d68fa56b3c26cb55aa24c688f3b42567ebf91a7
Skanner33 <mathewmageto@gmail.com>
2023-02-25 12:28:17 +0300

    added required methods and macros

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ActiveRecord::Base
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    # def print_all_reviews
+    #     self.reviews.each {|review|
+    #       puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+    #     }
+    # end
+
+    # def review(user_id)
+    #     self.reviews.find {|review| review.user_id == user_id}
+    # end
 end
+    
